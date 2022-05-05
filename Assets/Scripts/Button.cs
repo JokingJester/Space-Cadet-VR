8dc07eaaa286f8ae9b5939c2fd15d02ef839c5ca
JokingJester <81639209+JokingJester@users.noreply.github.com>
2022-05-05 12:44:56 -0500

    The ship can now move to the warp position

--- Assets/Scripts/Button.cs
+++ Assets/Scripts/Button.cs
@@ -6,7 +6,9 @@ public class Button : MonoBehaviour
 {
     [SerializeField] private Animator _anim;
     [SerializeField] private AudioSource _audioSource;
+
     public bool buttonPressed;
+    public bool underButtonCover;
     private bool _canPressButton;
 
     private void OnEnable()
@@ -29,12 +31,21 @@ public class Button : MonoBehaviour
 
     public void FlipSwitch()
     {
-        if(_canPressButton == true)
+        if (_canPressButton == true && underButtonCover == false)
         {
             _canPressButton = false;
             buttonPressed = true;
             _anim.ResetTrigger("Flash");
             _anim.SetTrigger("Flip");
+            _audioSource.Play();
+        }
+        
+        if(_canPressButton == true && underButtonCover == true)
+        {
+            _canPressButton = false;
+            buttonPressed = true;
+            _anim.ResetTrigger("Flash");
+            _anim.SetTrigger("Flip Under Cover");
             _audioSource.Play();
         }
     }
