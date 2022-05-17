2e185bfa61000253aff4be93afa38abb6ac47a8e
JokingJester <81639209+JokingJester@users.noreply.github.com>
2022-05-17 13:05:39 -0500

    Can Animate Hands When Pressing Trigger

--- Assets/Scripts/Player/Hand.cs
+++ Assets/Scripts/Player/Hand.cs
@@ -9,7 +9,9 @@ public class Hand : MonoBehaviour
     [SerializeField] private Animator _anim;
 
     private float _gripCurrent;
+    private float _triggerCurrent;
     private float _gripTarget;
+    private float _triggerTarget;
     [SerializeField] private float _speed;
     [SerializeField] private GameObject _handVisual;
     [SerializeField] private XRDirectInteractor _directInteractor;
@@ -24,12 +26,19 @@ public class Hand : MonoBehaviour
     void Update()
     {
         _gripTarget = _controller.selectAction.action.ReadValue<float>();
+        _triggerTarget = _controller.activateAction.action.ReadValue<float>();
         if(_gripCurrent != _gripTarget)
         {
             _gripCurrent = Mathf.MoveTowards(_gripCurrent, _gripTarget, Time.deltaTime * _speed);
         }
 
+        if (_triggerCurrent != _triggerTarget)
+        {
+            _triggerCurrent = Mathf.MoveTowards(_triggerCurrent, _triggerTarget, Time.deltaTime * _speed);
+        }
+
         _anim.SetFloat("Flex", _gripCurrent);
+        _anim.SetFloat("Pinch", _triggerCurrent);
     }
 
     public void ToggleHandVisual()
